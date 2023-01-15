1f6253e03c6f040a9999b5946c3b9dc3b87d6f74
Santiago L. Romero <santiago_pna@hotmail.com>
2023-01-15 19:29:48 -0300

    santi4246/Front-component-Home-Card-updated

--- client/src/Components/Card/index.jsx
+++ client/src/Components/Card/index.jsx
@@ -5,18 +5,20 @@ import style from './Card.module.css';
 export default function Card ({ game }) {
     return (
         <div className = {style.card}>
+            <NavLink to = {`home/game/${game.id}`} className={style.title}>
             <img src = {game.img} alt = {game.name} className = {style.img}></img>
-            <NavLink to = {`home/game/${game.id}`} className={style.title}>{game.name}</NavLink>
+            <h4>{game.name}</h4>
             <div className = {style.content}>
             {game.genres ? 
                     <>
                         {game.genres.map((genre, index) => (
                             <p key = { index }>{genre}</p>
-                        ))}
+                            ))}
                     </>
                 : <p>No genre</p>}
             <p>{game.rating} ⭐</p>
             </div>
+            </NavLink>
         </div>
     )
 }
