18f163eaa59b269818fd6cde731faa017a1c20d9
Santiago L. Romero <santiago_pna@hotmail.com>
2023-03-02 18:54:39 -0300

    Initial files v1.0.0

--- api/src/db.js
+++ api/src/db.js
@@ -9,11 +9,11 @@ const {
 const sequelize = new Sequelize(DB_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialectOptions: {
+  /* dialectOptions: {
     ssl: {
       require: true
     }
-  }
+  } */
 });
 const basename = path.basename(__filename);
 
